fac02426c5fdeb0a8609a984db405e36dd0aa790
grande19 <1b15agc@gmail.com>
2016-02-02 10:56:03 +0100

    Ejercicio hecho

--- ejercicio.py
+++ ejercicio.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+fichero_pass = open ("/etc/passwd")
+linea = fichero_pass.readline
+for linea in fichero_pass :
+	lista = linea.split(":")
+	print lista[0],lista[-1][:-1] #lista[:-1] afecta al resultado de lista[-1]
+	# print lista[0],lista[-1],   #otra forma 	
+
+print len(linea)
+
 
